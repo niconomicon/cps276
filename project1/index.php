8c0d715a9c11a912cba3d38d8b893ce32da8a0fa
niconomicon <nwilliam4@wccnet.edu>
2021-09-26 12:13:20 -0400

    formatted names and address fields

--- project1/index.php
+++ project1/index.php
@@ -14,27 +14,48 @@
     <div class="container">
         <form>
             <!--First & Last Name Fields-->
-            <div class="form-group">
-                <label for="fname">First Name</label>
-                <input type="text" class="form-control" id="fname">
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                        <label for="fname">First Name</label>
+                        <input type="text" class="form-control" id="fname">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="lname">Last Name</label>
+                        <input type="text" class="form-control" id="lname">
+                    </div>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="lname">Last Name</label>
-                <input type="text" class="form-control" id="lname">
-            </div>
-            <!--Address Fields-->
+            <!--Address Field-->
             <div class="form-group">
                 <label for="address">Address</label>
                 <input type="text" class="form-control" id="address">
             </div>
-            <div class="form-group">
-                <label for="city">City</label>
-                <input type="text" class="form-control" id="city">
+            <!--City, State, Zip Fields-->
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="city">City</label>
+                    <input type="text" class="form-control" id="city">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="state">State</label>
+                    <select id="state" class="form-control">
+                        <option selected>Choose...</option>
+                        <option>Connecticut</option>
+                        <option>New York</option>
+                        <option>Michigan</option>
+                        <option>Oregon</option>
+                        <option>California</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="inputZip">Zip</label>
+                    <input type="text" class="form-control" id="inputZip">
+                </div>
             </div>
-            <div class="form-group">
-                <label for="state">State</label>
-                <input type="text" class="form-control" id="state">
-            </div>
+            
             <!--Gender Radio Buttons-->
             <div class="form-group">
                 <label for="gender">Gender</label>
