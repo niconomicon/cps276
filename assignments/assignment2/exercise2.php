393ad506111db2b06621bf967f4a2785d77bb3fa
niconomicon <nwilliam4@wccnet.edu>
2021-10-02 12:00:02 -0400

    created assignment 2 structure

---
+++ assignments/assignment2/exercise2.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Exercise 2</title>
+</head>
+<body>
+</body>
+</html>
---
+++ notes/Notes_9_28.php
@@ -0,0 +1,42 @@
+<?php
+$var = "nicole";
+$val = "williams";
+
+$string="this is a string";
+echo strlen($string);
+
+echo "<br>";
+
+echo $string[strlen($string)-1];
+
+//This is a PHP comment
+
+//Use curly braces
+$favoriteAnimal = "cat";
+echo "My favorite animals are {$favoriteAnimal}s";
+/*This produces the expected result:
+My favorite animals are cats*/
+$i=0;
+$output = "";
+while ($1<10) {
+    $output .= $i."--";
+    $i++;
+}
+echo output;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta >
+    <title>PHP Basics</title>
+</head>
+<body>
+    <p>My name is <?php echo $var." ".$val;  ?></p> 
+    
+</body>
+
+</html>
