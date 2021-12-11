94e8f32ed45c05745bab732c6a9204acd468a5ac
niconomicon <nwilliam4@wccnet.edu>
2021-12-11 14:42:06 -0500

    made session login

--- assignments/finalassignment/pages/login.php
+++ assignments/finalassignment/pages/login.php
@@ -1,13 +1,32 @@
 <?php
 
 
-
-
 function init(){
-            
+    $error = '';
+
+if(isset($_POST['submit'])){
+    /* IF THE USERNAME AND PASSWORD MATCH THEN REDIRECT TO */
+    if($_POST['username'] === "admin" && $_POST['password'] === "password"){
+      
+      session_start();
+      $_SESSION['access'] = "accessGranted";
+  
+      /* HERE I STORE A FIRST NAME IN THE SESSION AS WELL AND WILL DISPLAY IT ON EVERY PAGE*/
+      $_SESSION['fname'] = $_POST['fname'];
+  
+      //session_regenerate_id();
+      header('location:index.php?page=welcome');
+    }
+    else {
+      $error = "Incorrect username or password";
+    }
+  }
+        
+    //$cats='hey look, cats';
+
     $login=<<<HTML
 
-        
+    <p class="error"> $error </p>   
     <form action="index.php"> 
     <div class="form-group">
         <label for="exampleInputEmail1">Email</label>
@@ -17,8 +36,8 @@ function init(){
         <label for="exampleInputPassword1">Password</label>
         <input type="password" class="form-control" id="exampleInputPassword1" placeholder="password">
     </div>
-    <!--<button type="submit" class="btn btn-primary" href="index.php?page=welcome" name="submit">Login</button>-->
-    <a class="btn btn-primary" href="index.php?page=welcome">Login</a>
+    <input type="submit" name="submit" value="Login" class="btn btn-primary">
+    <!-- <a class="btn btn-primary" href="index.php?page=welcome">Login</a> -->
     </form>
 HTML;
             
